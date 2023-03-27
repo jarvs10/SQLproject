e80c91f0f18727952a32f5cf9b73780c4e4e35e9
Jaiber Restrepo Vila <104040403+jarvs10@users.noreply.github.com>
2023-03-27 13:34:16 -0500

    update rutaM

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ const routes = createBrowserRouter([
         element: <Index />
       },
       {
-        path: '/lineam',
+        path: '/lineaM',
         element: <LineaM />
       },
       {
